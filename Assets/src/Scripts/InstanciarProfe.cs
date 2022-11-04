6bd4fb733e34be8ce588df5b2203c688eeaa7e2d
AleGV258 <ale.gv258@gmail.com>
2022-11-04 09:54:35 -0600

    Upd - Scripts

--- Assets/src/Scripts/InstanciarProfe.cs
+++ Assets/src/Scripts/InstanciarProfe.cs
@@ -19,10 +19,13 @@ public class InstanciarProfe : MonoBehaviour
     }
     public void instanciarPanelProfesor()
     {
-        GameObject PanelProfesorInstanciado = Instantiate(PanelProfe, new Vector3(0, 0,0), Quaternion.identity, GameObjectTarget.transform);
+        GameObject PanelProfesorInstanciado = Instantiate(PanelProfe, new Vector3(0, 0, 0), Quaternion.identity, GameObjectTarget.transform);
         PanelProfesorInstanciado.transform.parent = GameObjectTarget.transform; 
         // = GameObjectTarget.transform.position
         PanelProfesorInstanciado.SetActive(true);
     }
 
+    // Método para eliminar el objeto creado
+    // Hacer que el objeto este bien colocado (tamaños y coordenadas dinámicas)
+    // Probar con diferentes targets
 }
