f2fbc4c768f7c54d68a4e6081291bf1b4df95e7b
Michell GV <ale.gv258@gmail.com>
2023-02-10 11:39:34 -0600

    Upd - Croquis

--- Assets/src/Scripts/panelCroquis/OcultarCroquis.cs
+++ Assets/src/Scripts/panelCroquis/OcultarCroquis.cs
@@ -9,13 +9,55 @@ public class OcultarCroquis : MonoBehaviour
     public GameObject D; // GameObject de la imagen del edificio Laboratorios
     public GameObject B; // GameObject de la imagen del edificio Biblioteca
     public GameObject A; // GameObject de la imagen del edificio Aulas
+    public GameObject Mapa; // GameObject de la imagen del mapa de la facultad
 
-    // Start is called before the first frame update
+    // Función que se ejecuta al iniciar la aplicación y en su primer frame
     void Start()
     {
+        I.SetActive(true); // Se activa la imagen
+        D.SetActive(true); // Se activa la imagen
+        A.SetActive(true); // Se activa la imagen
+        B.SetActive(true); // Se activa la imagen
+        I.SetActive(false); // Se desactiva la imagen
+        D.SetActive(false); // Se desactiva la imagen
+        A.SetActive(false); // Se desactiva la imagen
+        B.SetActive(false); // Se desactiva la imagen
+        Mapa.SetActive(true); // Se activa la imagen
+    }
+
+    // Función que activa el GameObject de aula y desactiva los demás
+    public void activarAulas(){
         I.SetActive(false); // Se desactiva la imagen
         D.SetActive(false); // Se desactiva la imagen
         A.SetActive(true); // Se activa la imagen
         B.SetActive(false); // Se desactiva la imagen
+        Mapa.SetActive(false); // Se desactiva la imagen
+    }
+
+    // Función que activa el GameObject de biblioteca y desactiva los demás
+    public void activarBiblioteca(){
+        I.SetActive(false); // Se desactiva la imagen
+        D.SetActive(false); // Se desactiva la imagen
+        A.SetActive(false); // Se desactiva la imagen
+        B.SetActive(true); // Se activa la imagen
+        Mapa.SetActive(false); // Se desactiva la imagen
+    }
+
+    // Función que activa el GameObject de laboratorio y desactiva los demás
+    public void activarLaboratorios(){
+        I.SetActive(false); // Se desactiva la imagen
+        D.SetActive(true); // Se activa la imagen
+        A.SetActive(false); // Se desactiva la imagen
+        B.SetActive(false); // Se desactiva la imagen
+        Mapa.SetActive(false); // Se desactiva la imagen
+    }
+
+    // Función que activa el GameObject de inovacion y desactiva los demás
+    public void activarInovacion(){
+        I.SetActive(true); // Se activa la imagen
+        D.SetActive(false); // Se desactiva la imagen
+        A.SetActive(false); // Se desactiva la imagen
+        B.SetActive(false); // Se desactiva la imagen
+        Mapa.SetActive(false); // Se desactiva la imagen
     }
 }
